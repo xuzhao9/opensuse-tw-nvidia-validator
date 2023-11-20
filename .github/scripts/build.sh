471d134f0256b343fe9a36aa8e6c60d348dfa4e8
Xu Zhao <i@xuzhao.net>
2023-11-20 12:40:58 -0500

    Fix build and metadata scripts.

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Try building NVIDIA driver releases
-mkdir -p /workspace/.workdir/build
+mkdir -p $PWD/.workdir/build
 python3 ./opensuse_tw_nvidia_validator/nvidia-driver-inspector.py \
                   --metadata-json $PWD/.workdir/metadata.json \
                   --output-json $PWD/.workdir/build/build.json
-cat /workspace/.workdir/build/build.json
+cat $PWD/.workdir/build/build.json
--- .github/scripts/metadata.sh
+++ .github/scripts/metadata.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
 # Check the most recent 3 NVIDIA driver releases
+mkdir -p $PWD/.workdir
 python3 ./opensuse_tw_nvidia_validator/nvidia-driver-inspector.py -n 3 --download \
                   --metadata-json $PWD/.workdir/metadata.json
-cat /workspace/.workdir/metadata.json
+cat $PWD/.workdir/metadata.json
