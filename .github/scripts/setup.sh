7c46c6435154a379bf2d2abf370bc51e965016ca
Xu Zhao <i@xuzhao.net>
2023-11-20 13:09:12 -0500

    Add awk

--- .github/scripts/setup.sh
+++ .github/scripts/setup.sh
@@ -5,4 +5,4 @@ zypper in -y python3 python311-beautifulsoup4 python311-requests \
 	python311-urllib3 python311-distro
 
 # Install kernel deps
-zypper in -y make gcc kernel-default-devel kernel-devel git vim
+zypper in -y make gcc kernel-default-devel kernel-devel git vim awk
