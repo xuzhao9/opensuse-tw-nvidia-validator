996ca94eadaf6f4d0cbec206f1672b6b295e7afd
Xu Zhao <i@xuzhao.net>
2023-11-20 12:30:39 -0500

    Run setup

--- .github/scripts/setup.sh
+++ .github/scripts/setup.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+
+# Install python deps
 sudo zypper in -y python3 python311-beautifulsoup4 python311-requests python311-urllib3
+
+# Install kernel deps
+sudo zypper in -y make gcc kernel-default-devel kernel-devel git
