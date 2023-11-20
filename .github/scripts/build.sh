6c4c933d1174d5b86117cbb6e2500a96049a62cb
Xu Zhao <i@xuzhao.net>
2023-11-20 12:47:00 -0500

    Test build

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -3,5 +3,5 @@
 mkdir -p $PWD/.workdir/build
 python3 ./opensuse_tw_nvidia_validator/nvidia-driver-inspector.py \
                   --metadata-json $PWD/.workdir/metadata.json \
-                  --output-json $PWD/.workdir/build/build.json
+                  --build-json $PWD/.workdir/build/build.json
 cat $PWD/.workdir/build/build.json
